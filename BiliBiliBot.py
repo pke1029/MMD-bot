b2433eb265626b00e26de94f1ec52f35a3d868e5
PKE1029 <pangkhangee@gmail.com>
2018-05-15 13:02:05 +0100

    Login function
    Login function

--- BiliBiliBot.py
+++ BiliBiliBot.py
@@ -1,2 +1,17 @@
 import praw
+import config
+import time
 
+def bot_login():
+	r = praw.Reddit(username = config.username,
+					password = config.password,
+					client_id = config.client_id,
+					client_secret = config.client_secret,
+					user_agent = "Bilibili MMD Reddit bot v0.1")
+	print("Logged in")
+
+	return r
+
+
+## Main ------------------------------------------------------------
+r = bot_login()
