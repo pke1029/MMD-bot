aa062db008d4edd518cc30010d06d4acdee538c5
PKE1029 <pangkhangee@gmail.com>
2018-05-27 17:27:26 +0100

    bot_search function
    search for video containing "MMD" in title in Bilibili daily ranking "douga" category

--- BiliBiliBot.py
+++ BiliBiliBot.py
@@ -3,6 +3,7 @@ import config
 import time
 
 def bot_login():
+	print("Logging in...")
 	r = praw.Reddit(username = config.username,
 					password = config.password,
 					client_id = config.client_id,
@@ -13,5 +14,32 @@ def bot_login():
 	return r
 
 
-## Main ------------------------------------------------------------
-r = bot_login()
+
+# Main ------------------------------------------------------------
+
+#r = bot_login()
+
+
+import requests
+from bs4 import BeautifulSoup
+
+def bot_search():
+	# url for Bilibili "douga" category daily ranking
+	url = "https://www.bilibili.com/ranking/all/1/0/1"
+
+	# making soup
+	r = requests.get(url)
+	data = r.text
+	soup = BeautifulSoup(data, "lxml")
+	ranking = soup.find_all('div', class_ = "info")
+
+	# search for top submission with title containing "MMD" and get video id
+	for i in ranking:
+		if "MMD" in i.a.text.upper():
+			link = "https:" + i.a.get("href")
+			char_list = list(link)
+			video_id = "".join(char_list[-9:-1])
+			print(video_id)
+
+bot_search()
+
